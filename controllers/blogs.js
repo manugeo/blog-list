2699c9cc6c5217ae9e5ad18fa2ad37fc1cb0ceb0
Manu <manugeo13@gmail.com>
2022-06-03 13:13:44 +0530

    Updates to create blog endpoint.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,9 +7,11 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
-
-  const  result = blog.save();
+  const { title, author, likes, url } = request.body || {};
+  if (!title) return response.status(400).json({ error: "title is required." });
+  if (!url) return response.status(400).json({ error: "url is required." });
+  const blog = new Blog(Object.assign({}, { likes: 0 }, request.body));
+  const result = await blog.save();
   response.status(201).json(result);
 });
 
