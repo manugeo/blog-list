2f229bdafb2f906b8c400b4de773b391a3aca6dc
Manu <manugeo13@gmail.com>
2022-06-22 16:18:07 +0530

    Populating linked fields when creating blogs and users.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,34 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { userName: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, author, likes, url } = request.body || {};
+  const { title, author, likes, url, userId } = request.body || {};
   if (!title) return response.status(400).json({ error: "title is required." });
   if (!url) return response.status(400).json({ error: "url is required." });
 
+  const user = await User.findById(userId);
   const blog = new Blog({
     title,
     author,
     url,
-    likes: likes || 0
+    likes: likes || 0,
+    user: user?._id || null
   });
-  const result = await blog.save();
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+
+  if (user) {
+    // Note: updating the user document as well.
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+  }
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
