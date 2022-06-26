c70957f25ed9e6bd59aec2f228776317857a8ec7
Manu <manugeo13@gmail.com>
2022-06-26 21:36:13 +0530

    Implemeted token authentication

--- controllers/users.js
+++ controllers/users.js
@@ -3,19 +3,19 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
 usersRouter.post('/', async (request, response) => {
-  const { userName, name, password } = request.body;
+  const { username, name, password } = request.body;
 
-  if (!userName) return response.status(400).json({ error: 'Username is required.' });
+  if (!username) return response.status(400).json({ error: 'Username is required.' });
   if (!password) return response.status(400).json({ error: 'Password is required.' });
-  if (userName.length < 3) return response.status(400).json({ error: 'Username must be atleast 3 characters long.' });
+  if (username.length < 3) return response.status(400).json({ error: 'Username must be atleast 3 characters long.' });
   if (password.length < 3) return response.status(400).json({ error: 'Password must be atleast 3 characters long.' });
 
-  const existingUser = await User.findOne({ userName });
-  if (existingUser) return response.status(400).json({ error: `Username '${userName}' is already taken.` });
+  const existingUser = await User.findOne({ username });
+  if (existingUser) return response.status(400).json({ error: `Username '${username}' is already taken.` });
 
   const saltOrRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltOrRounds);
-  const user = new User({ userName, name, passwordHash });
+  const user = new User({ username, name, passwordHash });
   const savedUser = await user.save();
 
   response.status(201).json(savedUser);
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -32,25 +32,25 @@ describe('when there is initially one user in db', () => {
   beforeEach(async () => {
     await User.deleteMany({});
     const passwordHash = await bcrypt.hash('sekret', 10);
-    const user = new User({ userName: 'root', passwordHash });
+    const user = new User({ username: 'root', passwordHash });
     await user.save();
   });
 
   test('Creation succeeds with a valid username', async () => {
     const initialUsers = await helper.usersInDb();
-    const newUser = { userName: 'modera10', name: 'Modera Uchiha', password: 'sukiyomi' };
+    const newUser = { username: 'modera10', name: 'Modera Uchiha', password: 'sukiyomi' };
     await api.post('/api/users').send(newUser).expect(201).expect('Content-Type', /application\/json/);
 
     const usersAtEnd = await helper.usersInDb();
     expect(usersAtEnd).toHaveLength(initialUsers.length + 1);
 
-    const userNames = usersAtEnd.map(user => user.userName);
-    expect(userNames).toContain(newUser.userName);
+    const userNames = usersAtEnd.map(user => user.username);
+    expect(userNames).toContain(newUser.username);
   });
 
   test('creation fails with a proper status code and message if username already exists', async () => {
     const initialUsers = await helper.usersInDb();
-    const newUser = { userName: 'root', name: 'Super User', password: 'password' };
+    const newUser = { username: 'root', name: 'Super User', password: 'password' };
     const result = await api.post('/api/users').send(newUser).expect(400).expect('Content-Type', /application\/json/);
 
     expect(result.body.error).toContain("Username 'root' is already taken.");
